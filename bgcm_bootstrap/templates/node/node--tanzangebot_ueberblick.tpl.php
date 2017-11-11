56a54b4f26bd2db0da328e8c617d5ac9da9298f8
Michael Schumann <mschuma2@gmail.com>
2017-11-11 14:49:53 +0100

    note tanzangebot

---
+++ bgcm_bootstrap/templates/node/node--tanzangebot_ueberblick.tpl.php
@@ -0,0 +1,15 @@
+
+<div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+
+<!-- print first picture from gallery -->
+<img typeof="foaf:Image" src="<?php echo $GLOBALS['base_url'] ?>/sites/default/files/<?php print render($node->field_img_gallery['und'][0]['filename']); ?>" alt=""/>
+
+ <?php print render($node->body['de'][0]['safe_value']); ?>
+</div>
+
+
+
+
+
+
+    
